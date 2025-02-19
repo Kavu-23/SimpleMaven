b6ddf56ec9fa952f12d7624868af3d43a2bad1cc
Kavu-23 <kaveri.raddi@gmail.com>
2025-02-19 12:54:04 +0530

    Initial commit - Maven project setup

---
+++ src/test/java/SimpleTest/JavaTest.java
@@ -0,0 +1,14 @@
+package SimpleTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JavaTest {
+
+	 @Test
+	    public void testAddition() {
+	        int sum = 2 + 3;
+	        assertEquals(5, sum);
+	    }
+}
