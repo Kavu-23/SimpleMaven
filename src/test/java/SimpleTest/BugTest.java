fb37004c55e508fc8cac2e1e21a55416d0827601
Kavu-23 <kaveri.raddi@gmail.com>
2025-02-19 15:25:00 +0530

    bugTest1

--- src/test/java/SimpleTest/BugTest.java
+++ src/test/java/SimpleTest/BugTest.java
@@ -1,5 +1,32 @@
 package SimpleTest;
 
-public class BugTest {
+	import org.testng.Assert;
+	import org.testng.annotations.Test;
 
+	public class BugTest {
+
+	    // Bug #1: Hardcoded password (security issue)
+	    private String password = "123456";  
+
+	    // Bug #2: Unused variable
+	    private int unusedVariable = 42; 
+
+	    @org.junit.Test
+	    public void testAddition() {
+	        int result = add(2, 3);
+	        Assert.assertEquals(result, 6); // Bug #3: Wrong assertion (should be 5)
+	    }
+
+	    @org.junit.Test
+	    public void testStringNullCheck() {
+	        String text = null;
+	        if (text.equals("Hello")) {  // Bug #4: Possible NullPointerException
+	            System.out.println("Matched");
+	        }
+	    }
+
+	    public int add(int a, int b) {
+	        return a + b;
+	    }
+	
 }
